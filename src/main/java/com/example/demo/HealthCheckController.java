4f5c3d9ec9b1462a70c3dc176c6410fea5822c99
pywoo <kfdsy0103@naver.com>
2025-06-08 16:51:36 +0900

    FEAT: init repository

---
+++ src/main/java/com/example/demo/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+	@GetMapping("/health")
+	public String healthCheck() {
+		return LocalDateTime.now().toString();
+	}
+}
+
